211a2f818222a99d21e857fd62e8ce5efd4079d7
Divyam Aggarwal <mailtodivyam1@gmail.com>
2025-06-15 13:16:25 +0530

    add photo

--- app/(main)/dashboard/create-interview/_components/InterviewLink.jsx
+++ app/(main)/dashboard/create-interview/_components/InterviewLink.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-function InterviewLink({interview_id,formData}) {
+function InterviewLink({ interview_id, formData }) {
   return (
-    <div>
-      interview link
+    <div className='flex items-center justify-center mt-6'>
+      <Image src="/check1.webp" alt="check" width={200} height={200} className='w-[100px] h-[80px]' />
+
     </div>
-  )
+  );
 }
 
-export default InterviewLink
+export default InterviewLink;
