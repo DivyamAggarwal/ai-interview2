35b5229220c5e3740ba5dad5bb82c041a29727eb
Divyam Aggarwal <mailtodivyam1@gmail.com>
2025-06-17 18:52:38 +0530

    worked vapi

--- app/interview/[interview_id]/start/page.jsx
+++ app/interview/[interview_id]/start/page.jsx
@@ -2,10 +2,72 @@
 import { InterviewDataContext } from '@/context/InterviewDataContext';
 import { Mic, Phone, Timer } from 'lucide-react';
 import Image from 'next/image';
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import Vapi from "@vapi-ai/web"
+import AlertConfirmation from './_components/AlertConfirmation';
+
 
 function StartInterview() {
     const {interviewInfo,setInterviewInfo}=useContext(InterviewDataContext);
+    const vapi=new Vapi(process.env.NEXT_PUBLIC_VAPI_PUBLIC_KEY);
+    useEffect(()=>{
+      interviewInfo&& startCall();
+    },[interviewInfo])
+    const startCall=()=>{
+      let questionList="";
+      interviewInfo?.interviewData?.questionList?.forEach((item,index) => {
+        questionList=item?.question+","+questionList
+      });
+     const assistantOptions = {
+        name: "AI Recruiter",
+        firstMessage: "Hi"+ interviewInfo?.userName+", how are you? Ready for your interview on"+ interviewInfo?.interviewData?.jobPosition+"?",
+        transcriber: {
+          provider: "deepgram",
+          model: "nova-2",
+          language: "en-US",
+        },
+        voice: {
+          provider: "playht",
+          voiceId: "jennifer",
+        },
+        model: {
+          provider: "openai",
+          model: "gpt-4",
+          messages: [
+            {
+              role: "system",
+              content: `
+      You are an AI voice assistant conducting interviews.
+      Your job is to ask candidates provided interview questions, assess their responses.
+      Begin the conversation with a friendly introduction, setting a relaxed yet professional tone. Example:
+      "Hey there! Welcome to your `+ interviewInfo?.interviewData?.jobPosition+` interview. Let’s get started with a few questions!"
+      Ask one question at a time and wait for the candidate’s response before proceeding. Keep the questions clear and concise. Below Are the questions ask one by one:
+      Questions: `+questionList+`
+      If the candidate struggles, offer hints or rephrase the question without giving away the answer. Example:
+      "Need a hint? Think about how React tracks component updates!"
+      Provide brief, encouraging feedback after each answer. Example:
+      "Nice! That’s a solid answer."
+      "Hmm, not quite! Want to try again?"
+      Keep the conversation natural and engaging—use casual phrases like "Alright, next up..." or "Let’s tackle a tricky one!"
+      After 5-7 questions, wrap up the interview smoothly by summarizing their performance. Example:
+      "That was great! You handled some tough questions well. Keep sharpening your skills!"
+      End on a positive note:
+      "Thanks for chatting! Hope to see you crushing projects soon!"
+      Key Guidelines:
+      ✅ Be friendly, engaging, and witty ✅
+      ✅ Keep responses short and natural, like a real conversation
+      ✅ Adapt based on the candidate’s confidence level
+      ✅ Ensure the interview remains focused on React
+      `.trim(),
+            },
+          ],
+        },
+      };
+      vapi.start(assistantOptions)
+    }
+    const stopInterview=()=>{
+        vapi.stop();
+      }
   return (
     <div className='p-20 lg:px-48 xl:px-56'>
       <h2 className='font-bold text-xl flex justify-between'>AI Interview Session
@@ -27,7 +89,9 @@ function StartInterview() {
         </div>
         <div className='flex justify-center mt-6 items-center gap-6'>
             <Mic className='h-12 w-12 p-3 bg-gray-500 rounded-full text-white cursor-pointer'/>
-            <Phone className='h-12 w-12 bg-red-500 p-3 rounded-full text-white cursor-pointer'/>
+            <AlertConfirmation stopInterview={()=>stopInterview()}>
+              <Phone className='h-12 w-12 bg-red-500 p-3 rounded-full text-white cursor-pointer'/>
+            </AlertConfirmation>
         </div>
         <h2 className='text-sm text-gray-400 text-center mt-4'>Interview in Progress</h2>
     </div>
