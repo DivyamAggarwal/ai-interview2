4e8f2565aa1a50d2c61b7df284f194b1a398cb73
Divyam Aggarwal <mailtodivyam1@gmail.com>
2025-06-19 00:06:27 +0530

    feedback

--- services/Constants.jsx
+++ services/Constants.jsx
@@ -1,54 +1,54 @@
 import { BriefcaseBusinessIcon, Calendar, Code2Icon, LayoutDashboard, List, Puzzle, Settings, User2Icon, UserIcon, WalletCards } from "lucide-react";
 
-    export const SideBarOptions=[
-        {
-            name: 'DashBoard',
-            icon: LayoutDashboard,
-            path: '/dashboard'
-        },
-        {
-            name: 'Scheduled Interview',
-            icon: Calendar,
-            path: '/schedued-interview'
-        },
-        {
-            name: 'All Interview',
-            icon: List,
-            path: '/all-interview'
-        },
-        {
-            name: 'Billing',
-            icon: WalletCards,
-            path: '/billing'
-        },
-        {
-            name: 'Settings',
-            icon: Settings,
-            path: '/settings'
-        },
-    ]
-    export const InterviewType=[
-        {
-            name: 'Technical',
-            icon: Code2Icon,
-        },
-        {
-            name: 'Behavioral',
-            icon: User2Icon,
-        },
-        {
-            name: 'Exprience',
-            icon: BriefcaseBusinessIcon,
-        },
-        {
-            name: 'Problem Solving',
-            icon: Puzzle,
-        },
-        {
-            name: 'Leadership',
-            icon: UserIcon,
-        },
-    ]
+export const SideBarOptions=[
+    {
+        name: 'DashBoard',
+        icon: LayoutDashboard,
+        path: '/dashboard'
+    },
+    {
+        name: 'Scheduled Interview',
+        icon: Calendar,
+        path: '/schedued-interview'
+    },
+    {
+        name: 'All Interview',
+        icon: List,
+        path: '/all-interview'
+    },
+    {
+        name: 'Billing',
+        icon: WalletCards,
+        path: '/billing'
+    },
+    {
+        name: 'Settings',
+        icon: Settings,
+        path: '/settings'
+    },
+]
+export const InterviewType=[
+    {
+        name: 'Technical',
+        icon: Code2Icon,
+    },
+    {
+        name: 'Behavioral',
+        icon: User2Icon,
+    },
+    {
+        name: 'Exprience',
+        icon: BriefcaseBusinessIcon,
+    },
+    {
+        name: 'Problem Solving',
+        icon: Puzzle,
+    },
+    {
+        name: 'Leadership',
+        icon: UserIcon,
+    },
+]
     // export const QUESTIONS_PROMPT = `You are an expert technical interviewer.
     // Based on the following inputs, generate a well-structured list of high-quality interview questions:
     // Job Title: {{jobTitle}}
@@ -107,3 +107,23 @@ Respond **only** with a valid JSON object in this exact structure:
 - Keep language natural and professional.
 - give minimum 10 question or more than that
 `;
+export const FEEDBACK_PROMPT=`{{conversation}}
+Depends on this Interview Conversation between assitant and user,
+Give me feedback for user interview. Give me rating out of 10 for technical Skills,
+Communication, Problem Solving, Experince. Also give me summery in 3 lines
+about the interview and one line to let me know whether is recommanded
+for hire or not with msg. Give me response in JSON format
+{
+  feedback:{
+    rating:{
+      techicalSkills:5,
+      communication:6,
+      problemSolving:4,
+      experince:7
+    },
+    summery:<in 3 Line>,
+    Recommendation:"",
+    RecommendationMsg:""
+  }
+}
+`
