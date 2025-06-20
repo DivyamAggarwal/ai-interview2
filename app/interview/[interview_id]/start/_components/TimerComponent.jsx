35d36772d9d09e0113c339c5345e10b71967e85d
Divyam Aggarwal <mailtodivyam1@gmail.com>
2025-06-20 22:15:25 +0530

    timing comp

---
+++ app/interview/[interview_id]/start/_components/TimerComponent.jsx
@@ -0,0 +1,30 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+
+function TimerComponent({ start }) {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (start) {
+      interval = setInterval(() => {
+        setSeconds(prev => prev + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [start]);
+
+  const formatTime = (sec) => {
+    const h = String(Math.floor(sec / 3600)).padStart(2, '0');
+    const m = String(Math.floor((sec % 3600) / 60)).padStart(2, '0');
+    const s = String(sec % 60).padStart(2, '0');
+    return `${h}:${m}:${s}`;
+  };
+
+  return <span>{formatTime(seconds)}</span>;
+}
+
+export default TimerComponent;
